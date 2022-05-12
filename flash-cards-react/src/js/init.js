efeb6f6f9b89903be43755b2d3964a5c26f7f6cf
Noel Lastrella <noel.lastrella@gmail.com>
2022-05-11 23:20:34 -0400

    update template for cards object

--- flash-cards-react/src/js/init.js
+++ flash-cards-react/src/js/init.js
@@ -2,20 +2,22 @@
 
 export const Template = {
     score: 0,
-    card_types : [
+    cardTypes : [
         {
+            id: -1,
             type: "flip",
             title: "",
             front   :   {
-                text:   "",
+                text:   "NEW CARD FRONT",
                 media:  "" //URL : image or video
             },
             back    :   {
-                text : ""
+                text : "NEW CARD BACK"
             },
             answer: "",
             status: "incomplete", // incomplete, correct, incorrect
             points: 1,
+            correct: "n/a"
         },
         {
             type: "multiple choice",
