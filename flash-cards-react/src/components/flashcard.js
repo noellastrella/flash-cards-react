deb2fc01a1681e7083028f09503bc8657936cf6a
Noel Lastrella <noel.lastrella@gmail.com>
2022-05-11 23:19:17 -0400

    add flip icon, handle flip events, added content editable text, pass edit card function from parent to child

--- flash-cards-react/src/components/flashcard.js
+++ flash-cards-react/src/components/flashcard.js
@@ -1,28 +1,40 @@
 import React, { useState } from "react";
+import ContentEditable from 'react-contenteditable'
 
 function FlashCard(props){
     let flipped = "";
 
     const [cardStyle, setCardStyle] = useState("cont");
 
-    const editCard = (e) =>{
-        props.editCard(e)
-    }
 
     const flipCard = (e) =>{
         flipped = flipped == "" ? "flipped":"";
-        console.log(flipped)
         setCardStyle(flipped);
     }
 
 
+    const handleChangeBack = (e) => {
+        props.editCard(props.index, "Back", e.target.value)
+    }
+
+
+    const handleChangeFront = (e) => {
+        props.editCard(props.index, "Front", e.target.value)
+    }
+
+    let cardFront = props.getCardData(props.index).front.text;
+    let cardBack = props.getCardData(props.index).back.text;
+
+    console.log(props.getCardData(props.index))
     return(
-        <li className={`flashCard ${cardStyle}`} onClick={flipCard} >
+        <li className={`flashCard ${cardStyle}`}  >
             <div className="cardFront">
-                {props.textFront}
+                <img src="/images/flip.svg" class="flip-card" onClick={flipCard}/>
+                <ContentEditable html={cardFront}  onChange={handleChangeFront} />
             </div>
             <div className="cardBack">
-                {props.textBack}
+                <img src="/images/flip.svg" class="flip-card" onClick={flipCard}/>
+                <ContentEditable html={cardBack}  onChange={handleChangeBack} />
             </div>
         </li>
 
--- flash-cards-react/src/components/flashcards.js
+++ flash-cards-react/src/components/flashcards.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FlashCard from './flashcard';
-import { EditText, EditTextarea } from 'react-edit-text';
+
 
 
 function FlashCards(props){
@@ -12,7 +12,7 @@ function FlashCards(props){
             {
                 cards.map((e,i)=>{
                     return (
-                        <FlashCard key={"card-"+i} textFront={e.front.text} textBack={e.back.text}/>
+                        <FlashCard key={"card-"+i} editCard={props.editCard} getCardData={props.getCardData} index={i} textFront={e.front.text} textBack={e.back.text}/>
                     )
                 })
             }
